3dbe8b4ec411331f34fd62113588e767c825185c
andres-fm <andresfmcs97@gmail.com>
2016-08-28 22:32:39 -0500

    kangaroo challenge, Haskell version

---
+++ hacker-rank/algorithms/implementation/kangaroo.hs
@@ -0,0 +1,13 @@
+import Control.Applicative
+import Control.Monad
+import System.IO
+
+main :: IO ()
+main = do
+    x1_temp <- getLine
+    let x1_t = words x1_temp
+    let x1 = read $ x1_t!!0 :: Int
+    let v1 = read $ x1_t!!1 :: Int
+    let x2 = read $ x1_t!!2 :: Int
+    let v2 = read $ x1_t!!3 :: Int
+    if v1 > v2 && (((x2-x1) `mod`(v1-v2)) == 0) then putStr "YES" else putStr "NO"
